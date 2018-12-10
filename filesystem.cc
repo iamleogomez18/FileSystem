7d2da29b79a9fc036b738d134a3f66cd48c38427
Likhon Gomes <tug60499@temple.edu>
2018-12-10 00:11:16 -0500

    d

--- filesystem.cc
+++ filesystem.cc
@@ -59,7 +59,7 @@ typedef struct inodetable{
 void format(int fs);
 int makefile(char* path, char* filename);
 virtualfile* openfile(char* path, char* filename);
-int closefile(virtualfile *f);
+int closeFile(virtualfile *vf);
 int mkdir(char* path, char* dirname);
 int rm(char* path, char* name);
 int write(virtualfile* f, char* data);
@@ -68,8 +68,8 @@ int seek(virtualfile* f, int offset);
 int getFileNumber(char* path, char* filename);
 int addToInodeTable(inode file);
 inode readFromInodeTable(int filenum);
-void seektoempty();
-int seektodirectory(char* dirname);
+void seekEmpty();
+int seekDirectory(char* dirname);
 
 //global variables
 int fs;
@@ -183,7 +183,7 @@ int addToInodeTable(inode file) {
   strcat(rootstring, end);
   //write the inode to the virtualdisk
   lseek(fs, 0, SEEK_SET);
-  seektoempty();
+  seekEmpty();
   int result = write(fs, rootstring, strlen(rootstring));
   if(result == -1) {
     printf("Error writing inode.\n");
@@ -193,7 +193,7 @@ int addToInodeTable(inode file) {
  return 0;
 }
 
-void seektoempty() {
+void seekEmpty() {
   char* buf;
   do {
     read(fs, buf, 1);
@@ -257,10 +257,10 @@ void seektoempty() {
   }
   else {
     //the path specified isn't the root, so we have to find the correct directory    
-    seektodirectory(path);
+    seekDirectory(path);
   }
   //find next blank byte in the current directory to put the file pair
-  seektoempty();
+  seekEmpty();
   result = write(fs, filepair, strlen(filepair)*sizeof(char));
   //check if write to directory worked
   if(result == -1) {
@@ -277,7 +277,7 @@ void seektoempty() {
   return 0;
 }
 
-int seekToDirectory(char* path){
+int seekDirectory(char* path){
     char* pdup = strdup(path);
     char** pathArray = (char**) malloc(strlen(path)* sizeof(char));
     char* pathElement = strtok(pdup, "/");
@@ -342,4 +342,202 @@ inode readFromInodeTable(int fileNumber){
     in.editedTime = atoi(strtok(NULL,","));
     in.address = atoi(strtok(NULL,","));
     return in;
+}
+
+virtualfile* openFile(char* path, char* filename) {
+  cout << "openFile() called" << endl;
+  //get the file's address from its inode
+  int filenumber = getFileNumber(path, filename);
+  inode in = readFromInodeTable(filenumber);
+  int addr = in.address;
+  //navigate to file in myfs
+  int loc = lseek(fs, addr, SEEK_SET);
+  virtualfile *vf = (virtualfile*)malloc(sizeof(virtualfile));
+  vf->current = loc;
+  vf->start = loc;
+  vf->end = addr + BLOCK_SIZE -1;
+  printf("Opened virtualfile with vf.start = 0x%x,  vf.current = 0x%x and vf.end = 0x%x\n", vf->start, vf->current, vf->end);
+  return vf;
+}
+
+
+int closeFile(virtualfile* vf) {
+  cout << "closeFile() was called" << endl;
+  if(vf != NULL) {  
+    cout << "closed file at " << vf->start << endl;
+    vf = NULL;
+    return 0;
+  }
+  else {
+    cout << "could not close since no file is open " << endl;
+    return(EXIT_FAILURE);
+  }
+}
+
+//function to make directory
+int makeDir(char* path, char* dirname) {
+    cout << "makeDir() called";
+  //make sure file name is of valid length
+  if(strlen(dirname) < 3) {
+    cout << "Directory name " << dirname << " too short" << endl;
+    return 1;
+  }
+  else if(strlen(dirname) > FILENAME_MAX_SIZE) {
+    cout << "Directory name " << dirname << " too long" << endl;
+    return 1;
+  }
+  //find an empty memory spot for the file to live
+  int result;
+  int loc;
+  loc = lseek(fs, LOC_ROOT, SEEK_SET);
+  char buf[1];
+  //read the first byte of each memory location to see if it's available
+  read(fs, buf, 1);
+  while(strcmp(buf, "") != 0) {
+    loc = lseek(fs, BLOCK_SIZE - 1, SEEK_CUR);
+    read(fs, buf, 1);
+  }
+  cout << "Creating directory at " << loc << endl; 
+  //write a non-zero byte at start of block so it's known that this block is taken
+  lseek(fs, loc, SEEK_SET);
+  result = write(fs, "1", 1);
+
+  //create inode entry
+  inode file;
+  time_t current;
+  time(&current);
+  file.createTime = current;
+  file.editedTime = current;
+  file.isDir = 1;
+  file.filenum = ++nextFD;
+  file.address = loc;
+
+    cout << "Creating directory with inode: Created Time: " << file.createTime <<" editedTime " << file.editedTime << " isDir " << file.isDir << " File Number: " << file.filenum << " Address " << file.address << endl;
+  //add file pair to directory
+  //filepair is a string of "filename , #"
+  char* filepair = (char*)malloc((FILENAME_MAX_SIZE + 2)*sizeof(char) + sizeof(int));
+  char* filepairspacer = ",";
+  strcpy(filepair, filepairspacer);
+  strcat(filepair, dirname);
+  strcat(filepair, filepairspacer);
+  char* fnumstring = (char*)malloc(16);
+  sprintf(fnumstring, "%d", file.filenum);
+  strcat(filepair, fnumstring);
+  //if the path specified is the root (specified by "/" or just ""), navigate to it
+  if(strcmp(path,"/") == 0 || strcmp(path,"") == 0) {
+    loc = lseek(fs, LOC_ROOT, SEEK_SET);
+  }
+  else {
+    //the path specified isn't the root, so we have to find the correct directory    
+    seekDirectory(path);
+  }
+  //find next blank byte in the current directory to put the file pair
+  seekEmpty();
+  result = write(fs, filepair, strlen(filepair)*sizeof(char));
+  //check if write to directory worked
+  if(result == -1) {
+    cout << "Error writing directory " << dirname << " to virtual filysystem" << endl;
+    perror("Error writing directory in my_mkdir().");
+    return(EXIT_FAILURE);
+  }
+  else {
+    cout << "Successfully wrote directory " << dirname << " to " << path << endl;
+  }
+  //save directory inode to inode table in virtual filesystem
+  addToInodeTable(file);
+  return 0;
+}
+
+int rm(char* path, char* name) {
+  printf("\n---FUNCTION: my_rm()---\n");
+  seekDirectory(path);
+  char* currentdir = (char*)malloc(512);
+  read(fs, currentdir, 512);
+  //find (file, filenumber) pair so we can remove it 
+  char* filepair = strstr(currentdir, name);
+  if(filepair == NULL) {
+      cout << "Could not find " << name << " in " << currentdir;
+
+    return(EXIT_FAILURE);
+   }
+  //overwrite file name & number with \0
+  while(*filepair != ',') {
+    *filepair = '\0';
+    filepair++;
+  }
+  printf("Deleted entry for file '%s' in directory '%s'.\n", name, path);
+  return 0;
+}
+
+int write(virtualfile* f, char* data) {
+    cout << "write() called" << endl;
+  //navigate to current location within file
+  lseek(fs, f->current, SEEK_SET);
+  //make sure we aren't going to overflow into the next file
+  int bytesrem = f->end - f->current;
+  if(strlen(data) > bytesrem) {
+    cout << "Not enough memory for the file to write " << endl;
+    return(EXIT_FAILURE);
+  }
+  int result;
+  result = write(fs, data, strlen(data));  
+  //check if write to file worked
+  if(result == -1) {
+    cout << "Error writing " << data << " to file" << endl;
+    return(EXIT_FAILURE);
+  }
+  else {
+    cout << "successfully wrote data " << data << " to file" << endl;
+  }
+ 
+  //update current location in virtualfile
+  f->current = f->current + strlen(data);
+
+  return 0;
+}
+
+int read(virtualfile* f, char* buf, int size) {
+  cout << "read() called" << endl;
+  //navigate to current location within file
+  lseek(fs, f->current, SEEK_SET);
+  //check if we're reading past end of file
+  if(size > (f->end - f->current)) {
+    cout << "Error reading past end of file" << endl;
+    return(EXIT_FAILURE);
+  }
+  //read data into buf
+  cout << "Reading data from memory locations " << f->current << " to " << f->current + size;
+  read(fs, buf, size);
+  cout << " successfully read content " << buf << " from file" << endl;
+  //update f->current
+  f->current = f->current + size;
+  return 0;
+}
+// seeking to a location within a file, at a specified offset from the file's first byte
+int seek(virtualfile* f, int offset) {
+  lseek(fs, f->start, SEEK_SET);
+  int loc = lseek(fs, offset, SEEK_CUR);
+  //update f->current
+  f->current = loc;
+  return 0;
+}
+
+
+int getFileNumber(char* path, char* filename) {
+  seekDirectory(path);
+  char* currentdir = (char*)malloc(512);
+  read(fs, currentdir, 512);
+  //find (file, filenumber) pair so we can navigate to next level
+  char* filepair = strstr(currentdir, filename);
+  if(filepair == NULL) {
+    cout << "Error: Could not find " << filename << " in " << currentdir;
+    return(EXIT_FAILURE);
+   }
+  //get the associated file # of that file
+  while(*filepair != ',') {
+    filepair++;
+  }
+  filepair++;
+  int filenum = *filepair - '0';
+  return filenum;
 }
